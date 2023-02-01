9096a98d62909b7f9c5cac3147b4fe066b4a5c95
Vamshi Kumar Theegala <VamshiKumar.Theegala@ATMECS.CORP>
2023-02-01 12:32:35 +0530

    initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,27 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices,Browser } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000
+  },
+
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    
+    browserName : 'chromium',
+    headless : false
+ 
+  },
+
+};
+
+export default config;
---
+++ tests/tc1.spec.ts
@@ -0,0 +1,18 @@
+import { test,expect } from '@playwright/test';// here we are importing playwright test annotation
+test ('Login with invalid credentials using browser fixture', async({browser})=>{ 
+  //test is our test spec with an annonomous function 
+   // {browser} is a global fixture from playwright
+         const context = await browser.newContext(); 
+         // In this step we will open a new fresh browser with a method newContext()
+         const page = await context.newPage();
+         // A new fresh page opens to enter url
+         // await is a command used to wait until the browser and page opens.
+         await page.goto("https://rahulshettyacademy.com/loginpagePractise/") // goto is a method which invoke the url into the browser
+         console.log(await page.title);
+         await expect (page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy');
+         await page.locator('#username').type("rahulshetty");
+         await page.locator('#password').type("learning");
+         await page.locator('#signInBtn').click;
+         //console.log(await page.locator("[style='block']").textContent());
+       });
+
